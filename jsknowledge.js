17d60a07308af6454bfbe6cae65c2f462768dc71
sreemonta20 <sbhowmikcse08@gmail.com>
2022-03-24 00:22:36 +0600

    javascript practice notes 24.03.22 12.22 am

--- jsknowledge.js
+++ jsknowledge.js
@@ -1,4 +1,5 @@
-/// Function declaration and input object as parameter
+//node jsknowledge.js
+/// Function declaration
 function greeter(person){
     return "Hello, " + person;
 }
@@ -6,3 +7,31 @@ function greeter(person){
    var user = "Xavier";
    
    console.log(greeter(user));
+
+// Fat Arrow Function
+let getsum = a => a+a;
+console.log(getsum(3));
+
+
+// Callback Function (use of function expression)
+// Example 1
+const message = function() {  
+    console.log("This message is shown after 3 seconds");
+}
+ 
+setTimeout(message, 3000);
+
+// Example 2
+const resultFunction = function(){
+    return 'eat your vegetables!';
+}
+function createQuote(quote, callback){ 
+    var myQuote =  quote();
+    callback(myQuote); // 2
+  }
+  
+  function logQuote(quote){
+    console.log(quote);
+  }
+  
+  createQuote(resultFunction, logQuote); // 1
